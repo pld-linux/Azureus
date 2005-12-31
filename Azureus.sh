4c2991017f20e40a685db3d9220fb33a586489d1
Marcin Banasiak <marcin.banasiak@gmail.com>
2005-12-31 16:38:12 +0000

    - Azureus.jar moved to /usr/share/Azureus
    Changed files:
        Azureus.sh -> 1.2

--- Azureus.sh
+++ Azureus.sh
@@ -6,9 +6,7 @@ else
 	LIBDIR="/usr/lib"
 fi
 
-AZURDIR="$LIBDIR/Azureus"
-
-CLASSPATH="/usr/share/java/swt.jar:/usr/share/java/commons-cli.jar:/usr/share/java/log4j.jar:$AZURDIR/Azureus.jar"
+CLASSPATH="/usr/share/java/swt.jar:/usr/share/java/commons-cli.jar:/usr/share/java/log4j.jar:/usr/share/Azureus/Azureus.jar"
 
 java -cp $CLASSPATH -Djava.library.path=$LIBDIR/swt org.gudy.azureus2.ui.swt.Main
 
