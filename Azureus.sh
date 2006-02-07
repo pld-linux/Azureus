45235cf152c68a1337f21194ca2775b9cc676ec8
Marcin Banasiak <marcin.banasiak@gmail.com>
2006-02-07 22:07:26 +0000

    - added support for console UI
    Changed files:
        Azureus.sh -> 1.3

--- Azureus.sh
+++ Azureus.sh
@@ -1,12 +1,33 @@
 #!/bin/sh
 
+AZUHOME="$HOME/.Azureus"
+
 if [ `arch` == "x86_64" ]; then
 	LIBDIR="/usr/lib64"
 else
 	LIBDIR="/usr/lib"
 fi
 
+if [ -f $AZUHOME/pld.config ]; then
+	. $AZUHOME/pld.config
+else
+	if [ ! -e $AZUHOME ]; then
+		mkdir $AZUHOME
+	fi
+	
+	UI="swt"
+	
+	cat > $AZUHOME/pld.config << EOF
+# UI options:
+# - swt
+# - console
+
+UI="swt"
+
+EOF
+
+fi
+
 CLASSPATH="/usr/share/java/swt.jar:/usr/share/java/commons-cli.jar:/usr/share/java/log4j.jar:/usr/share/Azureus/Azureus.jar"
 
-java -cp $CLASSPATH -Djava.library.path=$LIBDIR/swt org.gudy.azureus2.ui.swt.Main
-
+java -cp $CLASSPATH -Djava.library.path=$LIBDIR/swt org.gudy.azureus2.ui.common.Main --ui=$UI
