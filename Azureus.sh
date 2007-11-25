93fe912e1871e62bcb701045e52947fe9a803878
Elan Ruusamäe <glen@pld-linux.org>
2007-11-25 15:31:32 +0000

    - pass args to app
    Changed files:
        Azureus.sh -> 1.5

--- Azureus.sh
+++ Azureus.sh
@@ -30,4 +30,4 @@ fi
 
 CLASSPATH="/usr/share/java/swt.jar:/usr/share/java/commons-cli.jar:/usr/share/java/log4j.jar:/usr/share/Azureus/Azureus.jar"
 
-exec java -cp $CLASSPATH -Djava.library.path=$LIBDIR/swt org.gudy.azureus2.ui.common.Main --ui=$UI
+exec java -cp $CLASSPATH -Djava.library.path=$LIBDIR/swt org.gudy.azureus2.ui.common.Main --ui=$UI ${1:+"$@"}
